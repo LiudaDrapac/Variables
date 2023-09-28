8dc9a1d3a976af0b7be91991aac78ea0b4217873
Ludmila Drapac <ludmila.drapac@nsih.local>
2023-09-28 15:00:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+import variables.Variables;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Current World Population - " +7811080815L);
+        System.out.println("Cars Produced This Year  - " +55235358);
+        System.out.println("First Letter In Alphabet - " +'A');
+        System.out.println("Java Is Amazing - " +true);
+        System.out.println("Pi - " +3.14159F);
+    }
+}
---
+++ src/variables/Variables.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class Variables {
+    long currentWorldPopulation = 7811080815L;
+     int cars = 55235358;
+    char firstLetter = 'A';
+     boolean jA = true;
+    float Pi = 3.14159F;
+
+}
